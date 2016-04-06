d29beb6b6c2c97ade22450eb9061c1f42d6b6606
Wishe Deom <wishedeom@gmail.com>
2016-04-05 21:03:41 -0400

    merging

--- src/common/BaseThread.java
+++ src/common/BaseThread.java
@@ -26,7 +26,7 @@ public class BaseThread extends Thread
 	/**
 	 * Preserves value across all instances
 	 */
-	public static int siNextTID = 1;
+	public static int siNextTID = 0;
 
 	/**
 	 * Our Thread ID
